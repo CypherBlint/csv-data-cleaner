4262f8ffe1f7bcabd455126d42c00bac2f074e0a
cypherblint <ritcheyj.0237290@gmail.com>
2026-05-08 20:05:11 -0500

    Created Print file Function; realized problem with dedicated file path and mult computers for testing

--- csv_cleanerv1.0.py
+++ csv_cleanerv1.0.py
@@ -2,7 +2,10 @@
 import csv
 
 def import_file():
-    file_path = r'c:\users\dominus\code\csv-data-cleaner\input\leads-100.csv'
+    #------------------------
+    # path changes with computer. fix this
+    #------------------------
+    file_path = r'\input\leads-100.csv'
 
     try:
         with open(file_path, 'r', newline="") as csv_file:
@@ -11,17 +14,23 @@ def import_file():
             # Move the following section to a new function later
             # Use loop to transfer csv_file to a variable that can survive
             # outside the 'with'
-            for row in read_csv:
-                print(row)
+            #for row in read_csv:
+            #    print(row)
     except FileNotFoundError:
         print("Error. File not found. Please check your file name and path.")
         read_csv = ""
+    return read_csv
 
+def print_file(read_csv):
+    for row in read_csv:
+        print(row)
+    return
     
 
 
 def main():
-    import_file()
+    read_csv = import_file()
+    print_file(read_csv)
     return
 
 if __name__ == "__main__":
